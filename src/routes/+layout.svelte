d670ba191530a8ad91b497f2fe98834b247b8fd3
Martin Larsson <martin.99.larsson@telia.com>
2023-10-31 18:55:48 +0100

    Repo init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import 'virtual:uno.css';
+	import '../reset.css';
+	import '../app.css';
+</script>
+
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<div class="flex h-screen w-screen flex-justify-center flex-items-center">
+	<h1>Welcome to METACraft</h1>
+</div>
