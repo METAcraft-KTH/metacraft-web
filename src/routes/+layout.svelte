3d05e56182b54bfe599529523b14e30a82acff5b
Martin Larsson <martin.99.larsson@telia.com>
2023-11-23 18:04:37 +0100

    Live map

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 	import TopBar from '$lib/widgets/TopBar.svelte';
 </script>
 
-<div class="h-full">
+<div class="h-full flex flex-col">
 	<TopBar />
 	<slot />
 </div>
--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -1,1 +1,1 @@
-Live map!
+<iframe class="flex-1" src="https://map.mc.datasektionen.se" />
