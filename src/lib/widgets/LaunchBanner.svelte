90faa058256d644ab2aad0c7bf40dabbfa53c215
Martin Larsson <martin.99.larsson@pm.me>
2023-12-02 17:15:05 +0100

    Update countdown

--- src/lib/widgets/LaunchBanner.svelte
+++ src/lib/widgets/LaunchBanner.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	const launchDate = new Date('2023-12-22:17:00:00'); // 18 svensk tid
+	const launchDate = new Date('2023-12-26:17:00:00'); // 18 svensk tid
 
 	let currentTime = Date.now();
 
@@ -19,7 +19,7 @@
 
 <div class="flex flex-col justify-center items-center h-100%">
 	<div class="bg-pink-500 p-6 b-rd-4 color-white m-8">
-		<div class="font-size-10">Lansering den 22a December!</div>
+		<div class="font-size-10">Lansering den 26e December!</div>
 		<div class="font-size-10">
 			{d}d {h}h {m}m {s}s
 		</div>
