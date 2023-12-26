f235f927dc8fe91e74e6827c6ada4d48eaa93058
Alvinn8 <42838560+Alvinn8@users.noreply.github.com>
2023-12-26 13:09:21 +0100

    Add GDPR statement

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,13 @@
 	import '../reset.css';
 	import '../app.css';
 	import TopBar from '$lib/widgets/TopBar.svelte';
+	import Hotlink from '$lib/widgets/Hotlink.svelte';
 </script>
 
 <div class="h-full flex flex-col">
 	<TopBar />
 	<slot />
+	<div style="text-align: center; margin-top: 2em;">
+		<a href="/gdpr">GDPR Statement</a>
+	</div>
 </div>
