7c5a2000a06e6085da795f9d9e0ce9d526f0ff2b
Hexakon <hex4nova@gmail.com>
2024-03-10 17:09:56 +0100

    3.0 page

---
+++ src/routes/3.0/+page.svelte
@@ -0,0 +1,138 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+  import { slide } from 'svelte/transition';
+
+  import Splash from "$lib/layout/standard/Splash.svelte";
+  import Main from "$lib/layout/standard/Main.svelte";
+  import Title from "$lib/layout/standard/Title.svelte";
+
+  import d10 from "./10_v3.png";
+  import d4 from "./4_sv.jpg";
+  import d3 from "./3daysleft.png";
+  import d2 from "./2daysleft.png";
+  import dlaunch from "./metacraftlaunch_data.png";
+
+	import TypeAndTime from "$lib/widgets/TypeAndTime.svelte";
+	import Header from "$lib/layout/Header.svelte";
+	import Footer from "$lib/layout/Footer.svelte";
+
+  let post = true;
+
+  let pageNumber = 4;
+
+  $: if ($page.url.hash) pageNumber = parseInt($page.url.hash);
+</script>
+
+<link href="https://fonts.googleapis.com/css2?family=Courier+Prime:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+
+<Header splashPage={true} />
+
+<!-- !! CUSTOM BG !! -->
+<style>
+	.custombg {
+		background-image: radial-gradient(
+        #271f1f70, 
+        #271f1ff0
+      ), url("$lib/images/splashes/main.webp")
+	}
+
+  .mono {
+    font-family: "Courier Prime", monospace;
+  }
+</style>
+<div class="w-100lvw h-100lvh fixed z--10 custombg bg-center-center bg-cover" />
+<!-- !! CUSTOM BG !! -->
+
+<!-- TODO: gör det här elementet till en layout? -->
+
+  <Title>3.0<br>GRAND LAUNCH</Title>
+
+  <!--
+  <p class="font-bold text-2xl text-white shadow-lg">SPELA NU!</p>
+  <div class="bg-black b-white b-2 b-solid py-1 px-3 text-center text-xl md:text-2xl mb-3 text-white mc7 shadow-lg">
+    mc.datasektionen.se
+  </div>
+  -->
+
+  <div class="flex flex-row text-bold h-8 md:h-16 text-xl md:text-3xl gap-1 w-100% max-w-200 text-green px-2">
+    <button class="b-green b-solid mono text-3xl md:text-5xl text-white bg-black cursor-pointer w-8 md:w-16 h-100% flex justify-center items-center" on:click={() => {pageNumber--; if (pageNumber < 0) pageNumber = 4}}>
+      {#if pageNumber > 0}←{/if}
+    </button>
+    <div class="b-green b-2 b-solid flex-1 text-bold mono bg-black h-100% flex justify-center items-center">
+      {#if pageNumber == 0}
+        T MINUS 10 DAGAR
+      {:else if pageNumber == 1}
+      T MINUS 4 DAGAR
+      {:else if pageNumber == 2}
+      T MINUS 3 DAGAR
+      {:else if pageNumber == 3}
+      T MINUS 2 DAGAR
+      {:else}
+        LAUNCH DAY
+      {/if}
+    </div>
+    <button class="b-green b-solid mono text-3xl md:text-5xl text-white bg-black cursor-pointer w-8 md:w-16 h-100% flex justify-center items-center" on:click={() => {pageNumber++; if (pageNumber > 4) pageNumber = 0}}>
+      {#if pageNumber < 4}→{/if}
+    </button>
+  </div>
+
+      
+  <div class="mb-24 w-100% max-w-200 flex flex-col items-center text-base">
+    {#if pageNumber == 0}
+      <img transition:slide src={d10} alt="i" class="w-100% b-white b-solid b-2 m-2">
+      <div transition:slide class="w-100% b-white b-solid b-2 bg-black text-neutral-200 p-4 flex flex-col gap-3">
+        <p class="text-2xl">Välkomna tillbaka, METAcrafters!</p>
+        <p><b>TLDR:</b> METAcraft öppnar 26 december på IP <span class="mono">mc.datasektionen.se</span> med en storslagen launch-event. [Gå med i Discorden](https://discord.gg/MttUbkmeyD) och [läs om servern](https://mc.datasektionen.se/) så missar du inget.</p>
+
+        <hr class="w-100%">
+        
+        <p>Det har varit två långa år (irl) utan mining och crafting här i sektionen, och efter våra galna events längtar vi bara ännu mer till den klassiska vanilla-upplevelsen. Nu är väntan äntligen över: <b>METAcraft gör snart sin storslagen återkomst!</b> 🎉</p> 
+        
+        <p>Ta med era vänner och träda in i en värld av under: samma spel ni älskar, med nya ansikte och gamla vänner, samt en hel del engagerande server-händelser som aldrig tidigare skådats! Utforska vad som har hänt under de sista 1 000 åren (i Minecraft), kriga för din plats i legenderna, starta upp ditt affärsimperium, eller konstruera ditt mästerverk, vare sig det är ett borg, en drake, eller en gigantisk labyrint. Vi garanterar att det kommer alltid finnas saker att göra i denna nya upplevelse: vi har fått fundera och slipa på det över ett helt millenium, trots allt!</p>
+        
+        <p><b>den 26 december öppnar METAcraft sin port till helt nya & spännande äventyr.</b> Den kommer börja med en launch-event där ni kommer bland annat få en ordentlig introduktion till allt som servern har att erbjuda, samt hemliga aktiviteter att utföra. Missa den inte! 🎂</p>
+        
+        <p>Gå med i vår <a href="https://discord.gg/MttUbkmeyD" target="_blank" class="text-green"><b>Discord server</b></a> så att du inte missar en enda händelse.</p>
+        <p>Server IP (Java 1.20.2): <span class="mono">mc.datasektionen.se</span></p>
+      </div>
+    {:else if pageNumber == 1}
+      <img transition:slide src={d4} alt="i" class="w-100% b-white b-solid b-2 m-2">
+      <div transition:slide class="w-100% b-white b-solid b-2 bg-black text-neutral-200 p-4 flex flex-col gap-3">
+        <p class="text-2xl">Endast 4 dagar återstår innan survivalservern öppnas.</p>
+        <p>Under tiden har vi rotat djupt i ett laboratorium, och funnit denna blueprint som kan vara av intresse...</p>
+      </div>
+    {:else if pageNumber == 2}
+      <img transition:slide src={d3} alt="i" class="w-100% b-white b-solid b-2 m-2">
+      <div transition:slide class="w-100% b-white b-solid b-2 bg-black text-neutral-200 p-4 flex flex-col gap-3">
+        <p class="text-2xl">Survivalservern öppnas om mindre än 72 timmar!</p>
+        <p>Vi har fått in en spännande rekommendation som kan gynna alla byteshandlare...</p>
+      </div>
+    {:else if pageNumber == 3}
+      <img transition:slide src={d2} alt="i" class="w-100% b-white b-solid b-2 m-2">
+      <div transition:slide class="w-100% b-white b-solid b-2 bg-black text-neutral-200 p-4 flex flex-col gap-3">
+        <p class="text-2xl">God jul, METAcraftare!</p>
+        <p>Ikväll råder det julefrid, men med mindre än 48 timmar kvar till launch tänker vi snabbt påminna om några client-side mods för att maxa spelupplevelsen!</p>
+        <p>Om du aldrig har använt mods förut eller använder den officiella Minecraft-launchern, så rekommenderar vi att använda Fabric Loader enligt följande instruktioner:</p>
+        <ol class="list-decimal ml-4">
+          <li>Ladda ner installationsprogrammet <a href="https://fabricmc.net/use/installer/" target="_blank" class="text-green">här</a> och följ instruktionerna för att sätta upp profilen för 1.20.2.</li>
+          <li>Installera Simple Voice Chat <a href="https://modrinth.com/plugin/simple-voice-chat" target="_blank" class="text-green">här</a>, plus ev. andra mods du vill ha.</li>
+          <li>Placera .jar-filen du just har laddat ner i mods-mappen under .minecraft.</li>
+          <li>Starta spelet med den nya Fabric-profilen!</li>
+        </ol>
+        <p>Det är såklart inte krav att spela med några mods, men det är starkt rekommenderat att åtminstone spela med Simple Voice Chat för den bästa upplevelsen, eftersom mycket av det vi planerat på servern bygger på att man kan höra varandra i spelet.</p>
+        <p>Om du använder en 3rd party launcher som MultiMC, Prism, Lunar, CurseForge etc, följ instruktionerna för din launcher på hur man installerar mods.</p>
+        <p>Vi ses på servern!</p>
+      </div>
+    {:else}
+      <img transition:slide src={dlaunch} alt="i" class="w-100% b-white b-solid b-2 m-2">
+      <div transition:slide class="w-100% b-white b-solid b-2 bg-black text-neutral-200 p-4 flex flex-col gap-3">
+        <p class="text-2xl">ÄR NI REDO?</p>
+        <p>Vi kommer börja med en Campusrundvandring (i minecraft). Där kommer vi gå igenom allt som är bra att känna till! Det kommer även finnas en liten frågesport där vinnaren kommer få ett försprång med resurser, så missa den inte!</p>
+        <p>Vi ses på servern!</p>
+        </div>
+    {/if}
+  </div>
+
+  <Footer />
+
+
