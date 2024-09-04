e15e7c7fa821e3defe7269a553626cb8913c4713
hexakon <hex4nova@gmail.com>
2024-09-04 12:37:24 +0200

    added "/sok" route for application form

---
+++ src/routes/sok/+server.ts
@@ -0,0 +1,6 @@
+import { redirect } from '@sveltejs/kit'
+
+/** @type {import('./$types').RequestHandler} */
+export const GET = async () => {
+  throw redirect(301, `https://docs.google.com/forms/d/e/1FAIpQLSdW2aAwY8oEpnpLE8GhgcmwEkcAQLobZMTNV3hLrUv2kxbMgw/viewform`)
+}
