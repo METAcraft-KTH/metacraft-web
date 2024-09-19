13f5d35a207121be2a4d9835b42788c9d969591b
Alvinn8 <42838560+Alvinn8@users.noreply.github.com>
2024-09-19 15:42:20 +0200

    Hotfix new qr code

--- src/routes/tv/logo/+page.svelte
+++ src/routes/tv/logo/+page.svelte
@@ -5,5 +5,5 @@
 
 <div class="w-screen h-screen bg-cover flex flex-col justify-center items-center gap-5vh" style={`background-image: url(${splash})`}>
     <div class="text-#ffff00 bg-#000c p-3vh py-3vh mc7 text-8vh">KTH:S STÖRSTA MINECRAFT-GEMENSKAP</div>
-    <img src="https://media.discordapp.net/attachments/1258482339940536401/1282438315840241787/frame.png?ex=66ebe181&is=66ea9001&hm=fc2dd86a169a31c3457b2d73447b29753f2a3180a8b2ef16b24e235c3fd29f5b&=&format=webp&quality=lossless" alt="qr" class="w-60vh aspect-square">
+    <img src="/qr.jpg" alt="qr" class="w-60vh aspect-square">
 </div>
