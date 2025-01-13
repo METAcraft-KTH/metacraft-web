2a580d1f402f047119bfc9c57de36d5d5bad3b53
hexakon <hex4nova@gmail.com>
2025-01-13 16:29:02 +0800

    3D effect on footer

--- src/lib/layout/Footer.svelte
+++ src/lib/layout/Footer.svelte
@@ -11,8 +11,17 @@
   }
 </style>
 
-<div id="footer" class="text-center py-10 px-3 w-100% text-base text-white font-bold" class:bg-trapdoor={bg}>
-  <p class="m-b2">METAcraft ©2023-2024</p>
+
+<div id="footer" class="text-center py-10 px-3 w-100% text-base text-white font-bold relative" class:bg-footer={bg}>
+  <div class="overflow-hidden absolute w-100% left-0 top--50px flex justify-center">
+    <span
+      style="background-image: url('https://minecraft.wiki/images/Mud_Bricks_(texture)_JE1_BE1.png');
+             transform: perspective(400px) rotateX(60deg) scaleX(3.085);
+             background-size: 2.5%;"
+      class="w-1920px h-70px inline-block flex-shrink-0">
+    </span>
+  </div>
+  <p class="m-b2">METAcraft ©2023-2025</p>
   <p class="m-b2">"Minecraft" samt tillhörande grafik är varumärkesregistrerat av Mojang Synergies AB</p>
   <!--<p><a href="/about" class:text-pink-300={!bg}>Om oss</a> ・ <a href="/discord" target="_blank" class:text-pink-300={!bg}>Discord-server</a> ・ <a href="/gdpr" class:text-pink-300={!bg}>GDPR Statement</a></p>-->
   <div class="flex flex-col md:flex-row gap-4 text-center sm:mt-0 justify-center items-center">
