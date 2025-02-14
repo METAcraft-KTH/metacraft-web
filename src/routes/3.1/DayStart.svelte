df35f7117e3e29316e73eda2545ff71c83e6d95d
hexakon <hex4nova@gmail.com>
2025-02-15 00:34:02 +0800

    readded 3.0 and 3.1 pages for archival sake

---
+++ src/routes/3.1/DayStart.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  export let href;
+</script>
+<a href={'#'+href} class="w-100% text-center text-2xl md:text-3xl text-pink-400 hover:text-pink-200 transition-colors mt-12 mb-5 px-5 block relative">
+  <div class="absolute top--16 left-0" id={href} />
+  <slot />
+</a>
---
+++ src/routes/3.1/Message.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export let iconUUID = "ddf3db336b394922bc55ac7709d3dc1f";
+  export let iconOnLeft = true;
+  export let head: string = "Untitled";
+  export let subhead: string = "Unknown date";
+</script>
+
+<section class="relative flex flex-row items-start mb-3 mx-1">
+  {#if iconOnLeft}
+    <img src={`https://mc-heads.net/avatar/${iconUUID}/64.png`} alt="playerhead" class="b-pink-9 b-2 b-solid hidden md:block mr-3">
+  {/if}
+  <div class="b-pink-9 b-2 b-solid p-3 md:p-4 pt-2 bg-black w-100%">
+    <h1 class="text-pink-500 text-xl md:text-2xl font-bold">{head}</h1>
+    <h2 class="text-pink-700 mb-3">{subhead}</h2>
+    <img src={`https://mc-heads.net/avatar/${iconUUID}/40.png`} alt="playerhead" class="block md:hidden absolute top-2 right-2" style="box-shadow: .125rem .125rem rgb(131 24 67);">
+    <slot />
+  </div>
+  {#if !iconOnLeft}
+    <img src={`https://mc-heads.net/avatar/${iconUUID}/64.png`} alt="playerhead" class="b-pink-9 b-2 b-solid hidden md:block ml-3">
+  {/if}
+</section>
