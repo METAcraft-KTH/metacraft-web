220e86cecf8f61d7b84409de940e8e06e73bc2f7
hexakon <hex4nova@gmail.com>
2025-03-25 23:33:18 +0100

    added "muted" to the tv autoplay mp4 (and fallback gif)

--- src/routes/tv/campus/+page.svelte
+++ src/routes/tv/campus/+page.svelte
@@ -1,8 +1,11 @@
 <!-- svelte-ignore a11y-media-has-caption -->
 <script lang="ts">
     import video from "./video.mp4";
+    import fallback from "./fallback.gif";
 </script>
 
-<video autoplay loop style="width: 100%; height: 100%">
+<video autoplay loop muted style="width: 100%; height: 100%">
     <source src={video} type="video/mp4">
+    <!-- fallback gif -->
+    <img src={fallback} alt="fallback gif">
 </video>
