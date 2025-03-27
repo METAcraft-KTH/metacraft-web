3358987a3432a88b4056b1cc2370dfdb2813df3b
hexakon <hex4nova@gmail.com>
2025-03-27 21:09:10 +0100

    itsinthetoybox

---
+++ src/routes/secret/itsinthetoybox/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import video from "./my-tutorial.mp4";
+</script>
+
+<!-- svelte-ignore a11y-media-has-caption -->
+<video controls style="width: 100%; height: 100%">
+    <source src={video} type="video/mp4">
+</video>
