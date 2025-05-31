10633767890ef0d109dc80512d04a6d907e20891
hexakon <hex4nova@gmail.com>
2025-05-31 21:14:29 +0200

    /youtube route added

---
+++ src/routes/youtube/_server.ts
@@ -0,0 +1,8 @@
+import { redirect } from '@sveltejs/kit'
+
+/** @type {import('./$types').RequestHandler} */
+export const GET = async () => {
+  throw redirect(301, `https://www.youtube.com/@METAcraft_KTH`)
+}
+
+// this is temp disabled because adapter-static doesnt allow use of +server files. reinstate this if we choose to not use adapter-static
