a45e72ca63404020b3ce282c7ea3be8af10edde0
Acuadragon100 <30689683+Acuadragon100@users.noreply.github.com>
2025-11-18 12:59:00 +0100

    Add the shop key resource pack

--- src/routes/resource-packs/+page.svelte
+++ src/routes/resource-packs/+page.svelte
@@ -15,6 +15,11 @@
 			The default METAcraft resource pack. Replaces pink with cerise and contains the textures for some GUIs.
 			<a href="http://metacraft.nu:25585/da0fea11-6c24-4735-83f0-f2975c381ec6">Download</a>
 		</div>
+		<div class="flex flex-col gap-2 flex-1 bg-book p-10 leading-tight">
+			<div class="mc10 text-2xl md:text-3xl">Shop Key</div>
+			A small resource pack containing a texture for the shop key.
+			<a href="http://metacraft.nu:25585/b2b09336-93c6-49a7-a6b3-f283e224f272">Download</a>
+		</div>
 	</div>
 </div>
 <Footer />
